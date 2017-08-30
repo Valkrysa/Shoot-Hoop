ba0f13ec6e34bd456850eb830071b65b7181821d
Alice Robinson <valkrysa@gmail.com>
2017-08-29 23:17:32 -0700

    through lecture 37, gimbal locks, ball shooting

---
+++ Assets/Scripts/BallLauncher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLauncher : MonoBehaviour {
+
+    public GameObject basketballPrefab;
+    public float ballSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1")) {
+            GameObject instance = Instantiate(basketballPrefab);
+            instance.transform.position = transform.position;
+            Rigidbody instanceRigidBody = instance.GetComponent<Rigidbody>();
+            instanceRigidBody.velocity = transform.forward * ballSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour {
+
+    public Camera childCamera;
+    public float rotationSpeed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+        childCamera = GetComponentInChildren<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed * -1f;
+
+        transform.localRotation = Quaternion.Euler(0, mouseX, 0) * transform.localRotation;
+        childCamera.transform.localRotation = Quaternion.Euler(mouseY, 0, 0) * childCamera.transform.localRotation;
+    }
+}
