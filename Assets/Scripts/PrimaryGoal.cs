9d0f40e810a6a82b666214e363c6037080b76ee7
Alice Robinson <valkrysa@gmail.com>
2017-08-31 23:28:03 -0700

    Through Lecture 51, correcting basket detection

---
+++ Assets/Scripts/PrimaryGoal.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimaryGoal : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("Collided");
+        Debug.Log(other);
+    }
+}
